a3fd00d7a958d5b6d19c6b2f4111beeb21613cfc
saeedaa06 <fathimaasaeeda06@gmail.com>
2026-03-08 12:52:56 +0530

    Delete easy/1757. Recyclable and Low Fat Products.sql

--- easy/1757. Recyclable and Low Fat Products.sql
+++
@@ -1,7 +0,0 @@
--- LeetCode 1757
--- Recyclable and Low Fat Products
--- https://leetcode.com/problems/recyclable-and-low-fat-products/description/
-
-SELECT PRODUCT_ID
-FROM PRODUCTS
-WHERE LOW_FATS='Y' AND RECYClABLE='Y';
