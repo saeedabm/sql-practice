a988bd4f3f06189c225fb1c4092afa22e5c124a8
Saeeda B M <fathimaasaeeda06@gmail.com>
2026-03-08 17:01:34 +0530

    Add files via upload

---
+++ pizza-sales/pizza_sales.sql
@@ -0,0 +1,73 @@
+
+create database pizza;
+create table orders(
+order_id int not null primary key,
+order_date date not null,
+order_time time not null
+);
+
+create table order_details (
+order_details_id int not null ,
+order_id int not null ,
+pizza_id text not null,
+quantity int not null,
+primary key(order_details_id)
+);
+
+select * from orders;
+
+-- Retrieve the total number of orders placed.
+select count(order_id) as total_orders from orders;
+
+-- Calculate the total revenue generated from pizza sales.
+SELECT 
+    ROUND(SUM(order_details.quantity * pizzas.price),
+            2) AS total_sales
+FROM
+    order_details
+        JOIN
+    pizzas ON pizzas.pizza_id = order_details.pizza_id;
+    
+select pizza_type_id from pizzas where price=(select max(price) from pizzas);
+
+
+-- Identify the highest-priced pizza.
+SELECT 
+    pizza_types.name, pizzas.price
+FROM
+    pizza_types
+        JOIN
+    pizzas ON pizzas.pizza_type_id = pizza_types.pizza_type_id
+ORDER BY pizzas.price DESC
+LIMIT 1;
+
+
+
+
+-- Identify the most common pizza size ordered.
+SELECT 
+    pizzas.size,
+    COUNT(order_details.order_details_id) AS order_count
+FROM
+    pizzas
+        JOIN
+    order_details ON pizzas.pizza_id = order_details.pizza_id
+GROUP BY pizzas.size
+ORDER BY order_count DESC
+LIMIT 1;
+
+
+-- List the top 5 most ordered pizza types along with their quantities.
+SELECT 
+    pizza_types.name, SUM(order_details.quantity) AS quantity
+FROM
+    pizza_types
+        JOIN
+    pizzas ON pizza_types.pizza_type_id = pizzas.pizza_type_id
+        JOIN
+    order_details ON order_details.pizza_id = pizzas.pizza_id
+GROUP BY pizza_types.name
+ORDER BY quantity DESC
+LIMIT 5;
+
+
